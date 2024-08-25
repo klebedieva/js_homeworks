f8fcc2a5198e5fb86c4c7f43ce6bad33c0340f95
karyna.lebedieva <karyna.lebedieva@gmail.com>
2024-08-25 11:58:26 +0200

    Added js_homework1 file

---
+++ js_homework1.js
@@ -0,0 +1,127 @@
+/* console.log('number' + 3 + 3); */ 
+/* 
+string + number1 + number2 = 'stringnumber1number2'
+Результут: 'number33' (type: string) 
+*/
+
+/* console.log(null + 3); */ 
+/* 
+null + number = 0 + 3 = 3 
+Результут: 3 (type: number)
+*/
+
+/* console.log(5&&'qwerty'); */ 
+/* 
+true + true = останнє true
+Рузультат: qwerty (type: string)
+*/
+
+/* console.log(+'40' + +'2' + 'hillel'); */ 
+/*
+1. +'40' + +'2' = 40 + 2 = 42
+2. 42 + 'hillel' = number + string = 'numberstring'
+Результат: '42hillel' (type: string)
+*/ 
+
+/* console.log('10' - 5 === 6); */ 
+/* 
+1. 10 - 5 = 5
+2. 5 === 6
+Результат: false (type: boolean) 
+*/ 
+
+/* console.log(true + false); */  
+/* 
+1 + 0 = 1
+Результат: 1 (type: number)
+*/
+
+/* console.log('4px' - 3); */
+/*
+string (number+letters) - number = NaN - mumber = NaN
+Результат: NaN (type: number)
+*/
+
+/* console.log('4'-3); */ 
+/* 
+4 - 3 = 1
+Результат: 1 (type: number)
+*/
+
+/* console.log('6' + 3 ** 0); */ 
+/* 
+1. 3 ** 0 = 1
+2. '6' + 1 = string + number = 'stringnumber'
+Результат: 61 (type: string)
+*/
+
+/* console.log('12' / 6); */ 
+/* number / number = 12 / 6
+Результат: 2 (type: number)
+*/
+
+/* console.log('10' + (5 === 6)); */ 
+/*
+1. 5 === 6 = false
+2. '10' + false = string + false = 'stringfalse'
+Результат: '10false' (type: string)
+*/ 
+
+/* console.log(null == ''); */ 
+/* null = string
+Результат: false (type: boolean)
+*/ 
+
+/* console.log(3 ** (9 / 3)); */ 
+/* 
+1. 9 / 3 = 3
+2. 3 ** 3 = 27
+Результат: 27 (type: number)
+*/ 
+
+/* console.log(!!'false' == !!'true'); */ 
+/* 
+1. !! string == !! string
+2. !! true == !! true
+3. ! false == ! false
+4. true == true
+Результат: true (type: boolean) 
+*/
+
+/* console.log(0 || '0' && 1); */
+/* 
+1. '0' && 1 = true && true = останнє true = 1
+2. 0 || 1 = false || true = true = 1
+Результат: 1 (type: number)
+*/
+
+/* console.log((+null == false) < 1); */
+
+/* 
+1. 0 == false 
+2. false == false (true)
+3. true < 1
+4. 1 < 1 = false 
+Результат: false (type: boolean)
+*/
+
+/* console.log(false && true || true); */
+/* 
+1. false && true = false
+2. false || true = true
+Результат: true (type: boolean) */
+
+/* console.log(false && (false || true)); */
+/* 
+1. false || true = true
+2. false && true = false
+Результат: false (type: boolean) 
+*/
+
+/* console.log((+null == false)) < 1 ** 5; */
+/* 
+1. +null == false = false == false
+2. 0 == 0
+3. 1 ** 5 = 1
+4. 0 < 1 = true 
+*/
